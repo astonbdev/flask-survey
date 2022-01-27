f6ff0380e8d0564bbe7c0378dc7629fb3537c5c8
Chuck Lui <luialex0@gmail.com>
2022-01-27 11:53:28 -0800

    finished step 3

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from dis import Instruction
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
@@ -7,3 +8,19 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+response = []
+
+
+@app.get('/')
+def show_survey():
+
+    return render_template("survey_start.html",
+     title=survey.title,
+     instruction=survey.instructions)
+
+@app.post('/begin')
+def show_question():
+
+    return render_template('question.html', question = survey.questions[0])
+
